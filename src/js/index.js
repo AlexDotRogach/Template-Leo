d0151ec70f8b9201bc71c4fad37251b04b04b11e
Alex <alex.rogach131@ukr.net>
2020-10-19 23:13:07 +0300

    finised the second section

--- src/js/index.js
+++ src/js/index.js
@@ -73,4 +73,98 @@ document.addEventListener('DOMContentLoaded', () => {
     
     nextTab(next);
     backTab(back);
+
+// main tabs
+    const items = document.querySelectorAll('.inform_items .inform_items__item'),
+          inform = document.querySelectorAll('.main_section .inform_main'),
+          screenWidth = window.screen.width;
+
+    document.querySelector('.inform_items').addEventListener('click', (e) => {
+
+        if (e.target.classList == 'inform_items') {
+            items.forEach((e, ind) => {
+                e.classList.remove('inform_items__item_active');
+                e.style.transform = 'translateY(0)'; 
+                inform[ind].style.display = 'none';
+                inform[0].style.display = 'block';
+                setTimeout(function() {
+                    inform[0].style.transform = 'translateX(0)'; 
+                },100);
+            });
+        }
+    });
+
+    if (screenWidth > 1050) {
+        items.forEach((item, indIte) => {
+
+            item.addEventListener('click', () => {
+       
+                items.forEach(e => {
+                    e.classList.remove('inform_items__item_active');
+                    e.style.transform = 'translateY(0)'; 
+                });
+
+                inform.forEach((item, indInfo) => {
+
+                    if (indIte == indInfo) {
+                        item.style.display = 'block';
+                        setTimeout(function() {
+                            item.style.transform = 'translateX(0)'; 
+                        },100);
+                    } else {
+                        item.style.transform = 'translateX(-2000px)'; 
+                        item.style.display = 'none';
+                    }
+                });
+
+                item.classList.toggle('inform_items__item_active');
+                animMainTabs(item);
+            });
+        });
+
+    } else {
+        items.forEach((item, indIte) => {
+
+            item.addEventListener('click', () => {
+
+                items.forEach(e => {
+                    e.classList.remove('inform_items__item_active'); 
+                });
+
+                inform.forEach((item, indInfo) => {
+                    
+                    item.style.transform = 'translateX(-2000px)'; 
+                    item.style.display = 'none';
+
+                    if (indIte == indInfo) {
+                        item.style.display = 'block';
+
+                        setTimeout(function() {
+                            item.style.transform = 'translateX(0)'; 
+                        },100);
+                    } 
+                });
+
+                item.classList.toggle('inform_items__item_active');
+            });
+        });
+    }
+
+    function animMainTabs(elem) {
+        let i = 0;
+
+        insideAnim();
+
+        function insideAnim() {
+
+            if (i >= 55) {
+                elem.style.transform = `translateY(53.5px)`;
+                return;
+            }  else {
+                elem.style.transform = `translateY(${i}px)`;
+                i += 8;
+                requestAnimationFrame(insideAnim);
+            }
+        }
+    }
 });
